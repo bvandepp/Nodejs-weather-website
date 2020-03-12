6fc7aacd4e0875aa74735ee7c153d17336d31cff
Vanderpuije <vanderpuijeb@SpringfieldPublicSchools.com>
2020-03-12 19:43:32 -0400

    forecast updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,10 +16,13 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       const temp = body.currently.temperature;
       const pre = body.currently.precipProbability;
+      const tempHigh = body.daily.temperatureHigh;
+      const tempLow = body.daily.temperatureLow;
 
       callback(
         undefined,
         `It is currently ${temp} degrees, and there is ${pre} % chance of rain.`
+        `The high today is ${tempHigh} degrees and the low today is ${tempLow} degrees.`
       );
     }
   });
