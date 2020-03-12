669a3f1c39e66756778a95494e958b51df1395e2
Vanderpuije <vanderpuijeb@SpringfieldPublicSchools.com>
2020-03-12 19:01:59 -0400

    steup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ form.addEventListener("submit", e => {
   errorMsg.textContent = "loading ...";
   result.textContent = '';
 
-  fetch("http://localhost:3000/weather?address=" + location).then(response => {
+  fetch("/weather?address=" + location).then(response => {
     response.json().then(data => {
       if (data.error) {
         errorMsg.textContent = data.error;
